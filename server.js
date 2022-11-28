5c647b7e5b198f682dc7b99636cb28b918decccc
Serjay Parks <75953992+iLOVEthecolorORANGE@users.noreply.github.com>
2022-11-28 17:42:47 -0500

    Create route

--- server.js
+++ server.js
@@ -6,10 +6,24 @@ const reactViews = require("express-react-views")
 app.set("view engine", "jsx");
 app.engine("jsx", reactViews.createEngine());
 
-// ======== New =======
+app.use(express.urlencoded({ extended: false }));
+
+
+// ======== New ========
 
 app.get("/logs/new", (req, res)=>{
     res.render('New')
+})
+
+// ========= Create ========
+
+app.post("/logs/", (req, res) => {
+    if (req.body.shipIsBroken === "on"){
+        req.body.shipIsBroken = true
+    } else {
+        req.body.shipIsBroken = false
+    }
+    res.send(req.body)
 })
 
 app.listen(PORT,()=>{
--- views/New.jsx
+++ views/New.jsx
@@ -4,7 +4,7 @@ class New extends React.Component {
   render() {
     return (
       <div>
-        <form action="/logs" method="POST">
+        <form action="/logs/" method="POST">
             Title:
             <input type="text" name="Title" /> <br />
             New Entry:
